b1b7d1df77caf3b6fc051414d937944b7878f753
jbbodart <jbbodart@yahoo.com>
2015-09-09 11:50:49 +0200

    Update install.sh
    - Removed arch repo list (moved to Dockerfile with reflector)
    - Set locale to en_US.UTF-8

--- install.sh
+++ install.sh
@@ -3,18 +3,10 @@
 # exit script if return code != 0
 set -e
 
-# update arch repo list with uk mirrors
-echo 'Server = http://archlinux.mirrors.uk2.net/$repo/os/$arch' > /etc/pacman.d/mirrorlist
-echo 'Server = http://mirror.cinosure.com/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
-echo 'Server = http://mirrors.manchester.m247.com/arch-linux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
-echo 'Server = http://www.mirrorservice.org/sites/ftp.archlinux.org/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
-echo 'Server = http://arch.serverspace.co.uk/arch/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
-echo 'Server = http://mirror.bytemark.co.uk/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
-
 # set locale
-echo en_GB.UTF-8 UTF-8 > /etc/locale.gen
+echo en_US.UTF-8 UTF-8 > /etc/locale.gen
 locale-gen
-echo LANG="en_GB.UTF-8" > /etc/locale.conf
+echo LANG="en_US.UTF-8" > /etc/locale.conf
 
 # add user "nobody" to primary group "users" (will remove any other group membership)
 usermod -g users nobody
